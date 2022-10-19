d7291fafeae0ab712bb570da818ed310a4b8ba6c
Amanda Lee <leeandama87@gmail.com>
2022-10-19 14:16:15 -0700

    Created function for generatePassword(). Added base for if 'statements' when confirming whether or not the user wants the specific characters in their random password.

--- Develop/script.js
+++ Develop/script.js
@@ -4,9 +4,44 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function genreratePassword() {
+function generatePassword() {
 
   var passLength = prompt("How long do you want your password to be? It should be no less than 8 characters and more than 128 characters.");
+
+  if(isNaN(passLength)) {
+    alert("Not a number. Try again!");
+    return(generatePassword());
+  }
+
+  if (passLength <= 8 || passLength >= 128) {
+    alert("Password does not meet the criteria!");
+    return(generatePassword());
+
+  }
+
+  var upperTrue = confirm("Would you like to include Uppercase letters?")
+
+  if(upperTrue) {
+
+  }
+
+  var lowerTrue = confirm("Would you like to include Lowercase letters?");
+  
+  if(lowerTrue) {
+
+  }
+
+  var numTrue = confirm("Would you like to include Numbers?");
+  
+  if(numTrue) {
+
+  }
+
+  var specialTrue = confirm("Would you like to include Special Characters?");
+  
+  if(specialTrue) {
+
+  }
 
 }
 
