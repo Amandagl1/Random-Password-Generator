0043ba1fb4d542de996a242aa2eb915f39a76390
Amanda Lee <leeandama87@gmail.com>
2022-10-21 16:20:54 -0700

    Created functions that will select random characters from the users selected arrays.

--- Develop/script.js
+++ Develop/script.js
@@ -7,32 +7,45 @@ var specialCharacters = "!@#$%^&*()<>?-=+_".toString().split("");
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Function 'randomSelector' is using min and max retrieve items in the array from the lowest to the highest values.
+// Function 'randomSelector' and function 'randomItem' selects random characters from the variable list(s).
+function randomSelector(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var random = Math.random();
+  return Math.floor(min * (1 - random) + random * max);
 
-function randomSelector() {
-  return Math.floor(Math.random());
+}
+
+function randomItem(list) {
+  return list[randomSelector(list.length)];
 }
 
 function generatePassword() {
-
+  // Length of the password determined by the user.
   var passLength = prompt("How long do you want your password to be? It should be no less than 8 characters and more than 128 characters.");
 
-  // If user cancels the prompt
+  // If user cancels the prompt.
   if (passLength === null) {
     return;
   }
 
+  // If user input is not a number (NaN).
   if(isNaN(passLength)) {
     alert("Not a number. Please try again.");
     return(generatePassword());
   }
 
+  // User input is invalid if the length is not within the range of >= 8 or <= 128.
   if (passLength <= 8 || passLength >= 128) {
     alert("Invalid. Please choose a password length between 8 and 128 characters.");
     return(generatePassword());
   }
 
 
-
+// variable where selected arrays will get pushed to if user confirms them.
   var chosenCharacters = []
 
 // 'if' statements confirming if the user wants to include the set of characters.
@@ -60,21 +73,22 @@ function generatePassword() {
     chosenCharacters.push(specialCharacters)
   }
 
+  // Default in the case that all 'if statements' are false. Creates their random password using the upperCase array.
   if (chosenCharacters === 0) {
     chosenCharacters.push(upperCase)
   }
 
-  var passwordCharacters = ""
+  var passwordCharacters = ""  
 
-  for (var i = 0; i < passLength; i++);
-    
+  // 'for loop' that selected a random character (characterItem) from the selected lists (listArray) and adds them to an empty string (passwordCharacters) using '+='.
+  for (var i = 0; i < passLength; i++) {
+    var listArray = randomItem(chosenCharacters);
+    var characterItem = randomItem(listArray);
+    passwordCharacters += characterItem;
+  }
 
-//   for (var i = 0; i < passLength; i++) {
-//     var randomSet = chosenCharacters(selectedCharacters[i]);
-//     passwordCharacters += randomSet;
-//   }
 
- return generatePassword
+ return passwordCharacters;
 }
 
 
